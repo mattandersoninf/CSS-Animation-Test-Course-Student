a1b8b8d823a69e598908d8e5789f073d5ea5ef34
Matt Anderson <matt.anderson.inf@gmail.com>
2020-01-20 20:37:43 -0500

    Removed teacher content
    Removed teacher content

--- app.js
+++ app.js
@@ -3,42 +3,50 @@
 // table container
 var animatedTable = document.querySelector("table.board");
 
-// change this vairable to trigger different pulse colors
-var pulseColor = 'peach';
 
 /*************************************************************
  *  ON LOAD ANIMATION
+ * 
+ *  DO NOT CHANGE
+ * 
  *  When the page is first loaded, show three random points  
  *  on the animated to demonstrate the pulse effect. 
  * ***********************************************************/
 function onLoad(){
-  pulsePeachColor(5, 5, 5);
+  pulseColor(5, 5, 5);
 }
 
 /*************************************************************
  *  ANIMATED TABLE EVENT LISTENER
+ * 
+ *  TBD
+ * 
  *  If the element pressed is a table cell then call the 
- *  pulsePeach function. The pulseDepth is randomly set to an 
+ *  pulseColor function. The pulseDepth is randomly set to an 
  *  integer between 1 and 4.
  * ***********************************************************/
 animatedTable.addEventListener("click", function(){
     
     if (event.target.tagName == "TD" ){
         
-        pulsePeachColor(event.target.id.substring(0, event.target.id.indexOf("-")), event.target.id.substring(event.target.id.indexOf("-")+1, event.target.id.length), Math.floor(Math.random()*4)+1);
+        // Math.floor(Math.random()*4)+1
+        pulseColor(event.target.id.substring(0, event.target.id.indexOf("-")), event.target.id.substring(event.target.id.indexOf("-")+1, event.target.id.length), 4);
     
     }
 
 });
 
 /*************************************************************
- *  PULSE PEACH FUNCTION
+ *  PULSE COLORS FUNCTION
+ * 
+ *  TBD
+ * 
  *  Given an x coordinate, y coordinate, and pulseDepth, add  
  *  animate-table-cell to the table cell at those coordinates,
  *  remove the animation class after 500ms, and call the pulse
  *  function with a decremented pulse depth.
  * ***********************************************************/
-function pulsePeachColor(rowNum, colNum, pulseDepth){
+function pulseColor(rowNum, colNum, pulseDepth){
     
     if(pulseDepth > -1 && verifyCoordinates(rowNum, colNum)){
 
@@ -52,12 +60,6 @@ function pulsePeachColor(rowNum, colNum, pulseDepth){
 
             },500);
 
-            window.setTimeout(function() {
-                pulsePeachColor(parseInt(rowNum,10)+1,parseInt(colNum,10),pulseDepth-1);
-                pulsePeachColor(parseInt(rowNum,10),parseInt(colNum,10)+1,pulseDepth-1);
-                pulsePeachColor(parseInt(rowNum,10)-1,parseInt(colNum,10),pulseDepth-1);
-                pulsePeachColor(parseInt(rowNum,10),parseInt(colNum,10)-1,pulseDepth-1); 
-            },200); 
 
         }
 
@@ -68,6 +70,9 @@ function pulsePeachColor(rowNum, colNum, pulseDepth){
 
 /*************************************************************
  *  VERIFY COORDINATES FUNCTION
+ * 
+ *  TBD
+ * 
  *  Check to see if the given coordinates correspond to an 
  *  existing element that has an id attribute with these
  *  coordinates.
